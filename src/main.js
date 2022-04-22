c8614bf1db9e715d7f94158a820d8f3b39f9d179
relayer464 <mark-ford@metalexa.com>
2022-04-22 16:46:13 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,40 @@
+module.exports = (vemto) => {
+
+    return {
+
+        canInstall() {
+            return true
+        },
+
+        onInstall() {
+            vemto.savePluginData({
+                text: 'Hello world!!'
+            })
+        },
+
+        beforeCodeGenerationStart() {
+            let data = vemto.getPluginData()
+
+            vemto.log.info(data.text)
+            vemto.log.warning(`That's awesome!!! A Vemto plugin showing a message during code generation`)
+        },
+
+
+
+        beforeCodeGenerationEnd() {
+
+            let models = vemto.getProjectModels()
+
+            models.forEach(model => {
+                let options = {}
+                options.data = {model}
+
+                vemto.renderTemplate('/files/ListView.vemtl', '/resources/views/app/' + model.table + '/list.blade.php', options)
+
+            })
+        },
+
+
+    }
+
+}
